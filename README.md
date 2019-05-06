cd6f145e54716057676c1c6963dd69971aa48543
kerim.seymen <kerim.seymen@veribase.com>
2019-05-06 13:17:21 +0300

    Django First Kommit

---
+++ README.md
@@ -0,0 +1,3 @@
+"# blogapp" 
+"# benim blogum" 
+"# myblog" 
---
+++ article/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Article,Comment
+
+# Register your models here.
+admin.site.register(Comment)
+#admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display=["title","author","created_date"]
+
+    list_display_links=["title","author","created_date"]
+
+    search_fields=["title"]
+
+    #list_filter=["created_date"]
+    list_filter=["title"]
+
+    class Meta:
+        model=Article
+
+
+
